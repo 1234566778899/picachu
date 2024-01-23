a690fae4a8b67afd5b61b05308088059e1a930ac
Carlos Ordaz <carlos.ordaz@acomo.com.pe>
2024-01-23 00:36:25 -0500

    cc

--- src/components/HomeApp.jsx
+++ src/components/HomeApp.jsx
@@ -1,39 +1,57 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { MAPA } from '../utils/mapa'
 import '../styles/Home.css'
+
 export const HomeApp = () => {
     const [mapa, setmapa] = useState(MAPA);
-    const [player, setplayer] = useState([0, 6]);
-    const [direcciones, setDirecciones] = useState([[1, 0], [0, 1], [-1, 0], [0, -1]]);
+    const [player, setplayer] = useState({ i: 6, j: 0 });
+    const [direcciones, setdirecciones] = useState([
+        { dj: 1, di: 0 },
+        { dj: 0, di: 1 },
+        { dj: -1, di: 0 },
+        { dj: 0, di: -1 },
+    ])
     const [actual, setActual] = useState(0);
-    const [cantidad, setCantidad] = useState('');
-    const [sentido, setSentido] = useState(1); 
-    const moverPicachu = () => {
-        let paso = actual;
-        for (let i = 0; i < cantidad; i++) {
-            let encontro = false;
-            while (!encontro) {
-                let x = direcciones[paso][0];
-                let y = direcciones[paso][1];
-                if (mapa[player[0] + x] && mapa[player[0] + x][player[1] + y] == 5) {
-                    setplayer(prev => ([prev[0] + x, prev[1] + y]));
-                    encontro = true;
-                } else {
-                    paso = paso + 1 > 3 ? 0 : paso + 1;
-                    if ((-direcciones[actual][0] == direcciones[paso][0]) || (-direcciones[actual][1] == direcciones[paso][1])) {
-                        paso = paso + 1 > 3 ? 0 : paso + 1;
+    const [cantidad, setCantidad] = useState(0)
+
+    const moverPicachu = (cant, last, current) => {
+        try {
+            if (cant < cantidad) {
+                let valido = false;
+                let aux = current;
+                let next = {};
+                while (!valido) {
+                    let direccion = direcciones[aux];
+                    if (mapa[last.i + direccion.di] && mapa[last.i + direccion.di][last.j + direccion.dj] == 5) {
+                        next = { j: last.j + direccion.dj, i: last.i + direccion.di };
+                        valido = true;
+                    } else {
+                        aux = aux + 1 > 3 ? 0 : aux + 1;
+                        if (direcciones[aux].di == -direcciones[current].di || direcciones[aux].dj == -direcciones[current].dj) {
+                            aux = aux + 1 > 3 ? 0 : aux + 1;
+                        }
+
                     }
+
                 }
+                setplayer({ ...next });
+                setActual(aux);
+                cant++;
+                setTimeout(() => {
+                    moverPicachu(cant, next, aux);
+                }, 400);
             }
+
+        } catch (error) {
+            console.log(error);
         }
-        setActual(paso);
     }
 
     return (
         <div className='p-3'>
             <div className='panel'>
                 <input type="text" value={cantidad} onChange={(e) => setCantidad(e.target.value)} />
-                <button className='ms-2' onClick={() => moverPicachu()}>Mover</button>
+                <button className='ms-2' onClick={() => moverPicachu(0, player, actual)}>Mover</button>
             </div>
             <div className="map">
                 {
@@ -41,7 +59,7 @@ export const HomeApp = () => {
                         <div className="fila" key={idY}>
                             {
                                 y.map((x, idX) => (
-                                    <div key={`${idY}${idX}`} className={`columna ${x == 1 ? 'rojo' : (x == 3 ? 'verde' : (x == 4 ? 'amarillo' : (x == 2 ? 'azul' : (x == 5 ? 'camino' : ''))))} ${(idY == player[1] && idX == player[0]) ? 'player' : ''}`}>{ }</div>
+                                    <div key={`${idY}${idX}`} className={`columna ${x == 1 ? 'rojo' : (x == 3 ? 'verde' : (x == 4 ? 'amarillo' : (x == 2 ? 'azul' : (x == 5 ? 'camino' : ''))))} ${(idY == player.i && idX == player.j) ? 'player' : ''}`}>{ }</div>
                                 ))
                             }
                         </div>
