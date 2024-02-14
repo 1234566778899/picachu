6c8b87176230cea6cd3349434892ed5aa934b519
Carlos Ordaz <carlos.ordaz@acomo.com.pe>
2024-02-13 21:18:34 -0500

    cc

--- src/components/HomeApp.jsx
+++ src/components/HomeApp.jsx
@@ -4,17 +4,18 @@ import '../styles/Home.css'
 
 export const HomeApp = () => {
     const [mapa, setmapa] = useState(MAPA);
-    const [player, setplayer] = useState({ i: 6, j: 0 });
+    const [players, setplayers] = useState([{ i: 6, j: 0 }, { i: 0, j: 6 }]);
+    const [turno, setTurno] = useState(0);
     const [direcciones, setdirecciones] = useState([
         { dj: 1, di: 0 },
         { dj: 0, di: 1 },
         { dj: -1, di: 0 },
         { dj: 0, di: -1 },
     ])
-    const [actual, setActual] = useState(0);
     const [cantidad, setCantidad] = useState(0)
 
     const moverPicachu = (cant, last, current) => {
+
         try {
             if (cant < cantidad) {
                 let valido = false;
@@ -34,24 +35,40 @@ export const HomeApp = () => {
                     }
 
                 }
-                setplayer({ ...next });
-                setActual(aux);
+                const newplayers = players;
+                newplayers[turno] = next;
+                setplayers([...newplayers]);
                 cant++;
                 setTimeout(() => {
                     moverPicachu(cant, next, aux);
                 }, 400);
+            } else {
+                setTurno(prev => (prev + 1 < players.length ? prev + 1 : 0));
             }
 
         } catch (error) {
             console.log(error);
         }
     }
-
+    const getPlayer = (x, y) => {
+        for (const player of players) {
+            if (player.i == y && player.j == x) {
+                return true;
+            }
+        }
+        return false;
+    }
     return (
         <div className='p-3'>
-            <div className='panel'>
-                <input type="text" value={cantidad} onChange={(e) => setCantidad(e.target.value)} />
-                <button className='ms-2' onClick={() => moverPicachu(0, player, actual)}>Mover</button>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <div className='panel'>
+                    <input type="text" value={cantidad} onChange={(e) => setCantidad(e.target.value)} />
+                    <button className='ms-2' onClick={() => moverPicachu(0, players[turno], 0)}>Mover</button>
+                </div>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <span>Turno:</span>
+                    <span className='ms-2 fs-1'>{turno + 1}</span>
+                </div>
             </div>
             <div className="map">
                 {
@@ -59,7 +76,7 @@ export const HomeApp = () => {
                         <div className="fila" key={idY}>
                             {
                                 y.map((x, idX) => (
-                                    <div key={`${idY}${idX}`} className={`columna ${x == 1 ? 'rojo' : (x == 3 ? 'verde' : (x == 4 ? 'amarillo' : (x == 2 ? 'azul' : (x == 5 ? 'camino' : ''))))} ${(idY == player.i && idX == player.j) ? 'player' : ''}`}>{ }</div>
+                                    <div key={`${idY}${idX}`} className={`columna ${x == 1 ? 'rojo' : (x == 3 ? 'verde' : (x == 4 ? 'amarillo' : (x == 2 ? 'azul' : (x == 5 ? 'camino' : ''))))} ${getPlayer(idX, idY) ? 'player' : ''}`}>{ }</div>
                                 ))
                             }
                         </div>
